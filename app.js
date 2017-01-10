995fc088e2ce0159299a7f9484bbed7bacdf0d38
Shelly Duggal <shellduggal@gmail.com>
2017-01-09 21:00:41 -0600

    Start styling, Add OpenWeather icons

--- app.js
+++ app.js
@@ -29,10 +29,46 @@ function checkFahrenheit(position) {
 			// 1/2. Get city and country from API
 			$("#local").html(json["name"] + ', ' + json["sys"].country);
 			// 3. Get F temp from API
-    		$('#temp').html(json["main"].temp + "° F");
+    		$('#temp').html(Math.round(json["main"].temp) + "° F");
 			// 4. Get description from API
     		$('#skies').html(json["weather"][0].description);
-		});
+    		// 5. Get icon from API
+    		var iconSource = "http://openweathermap.org/img/w/" + json["weather"][0].icon + ".png"
+    		$('#icon').html('<img src=' + iconSource+ '>');
+    		// 6. Update background image
+    		switch(json["weather"][0].icon) {
+			  case "01d":
+			  case "01n":
+			  	$("body").css("background-image", "url:'https://cdn.pixabay.com/photo/2012/03/04/00/01/background-21717_1280.jpg'");
+			  	break;
+			  case "02d":
+			  case "02n":
+			  case "03d":
+			  case "03n":
+			  case "04d":
+			  case "04n":
+			  	$("body").css("background-image", "url:'https://cdn.pixabay.com/photo/2013/11/14/20/23/clouds-210649_1280.jpg'");
+			  	break;
+			  case "09d":
+			  case "09n":
+			  case "10d":
+			  case "10n":
+			  	$("body").css("background-image", "url:'https://cdn.pixabay.com/photo/2015/08/02/15/05/drip-871152_1280.jpg'");
+			  	break;
+			  case "11d":
+			  case "11n":
+			  	$("body").css("background-image", "url:'https://cdn.pixabay.com/photo/2016/10/25/12/28/california-1768742_1280.jpg'");
+				break;
+			  case "13d":
+			  case "13n":
+			  	$("body").css("background-image", "url:'https://cdn.pixabay.com/photo/2015/01/16/18/39/kermit-601711_1280.jpg'");
+			  	break;
+			  case "50d":
+			  case "50n":
+			  	$("body").css("background-image", "url:'https://cdn.pixabay.com/photo/2015/08/15/00/58/mountains-889131_1280.jpg'");
+			  	break;
+			}
+    });
 }
 
 function checkCelsius(position) {
@@ -40,9 +76,11 @@ function checkCelsius(position) {
 			// 1/2. Get city and country from API 
 			$("#local").html(json["name"] + ', ' + json["sys"].country);
 			// 3. Get F temp from API
-    		$('#temp').html(json["main"].temp + "° C");
+    		$('#temp').html(Math.round(json["main"].temp) + "° C");
 			// 4. Get description from API
     		$('#skies').html(json["weather"][0].description);
+    		var iconSource = "http://openweathermap.org/img/w/" + json["weather"][0].icon + ".png"
+    		$('#icon').html('<img src=' + iconSource+ '>');
 		});
 }
 
