296ae893b9099808e1bb968339377dee388b6219
Shelly Duggal <shellduggal@gmail.com>
2017-01-13 14:21:08 -0600

    Fix zip code lookup, Add extra weather data, Add error handling

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var weatherData;
 
+//function to check weather by geolocation API
 function getWeather() {
 	//Check if browser supports geolocation
 	if (navigator.geolocation) {
 		var timeoutVal = 10 * 1000 * 1000;
 		navigator.geolocation.getCurrentPosition(function(position) {
+			//retrieve data from API, store in weatherData object
 			$.ajax({
 				url: "http://api.openweathermap.org/data/2.5/weather",
+				//data: to be sent to API server
 				data: {
 					lat: position.coords.latitude,
 					lon: position.coords.longitude,
@@ -15,6 +18,7 @@ function getWeather() {
 				},
 				async: true,
 				cache: false,
+				//data to ask from API server
 				success: function(data) {
 					weatherData = {
 						"city": data["name"],
@@ -25,41 +29,50 @@ function getWeather() {
 						"icon": data["weather"][0].icon,
 						"pressure": data["main"].pressure,
 						"humidity": data["main"].humidity,
-						"windSpeed": data["wind"].speed,
-						"windDir": data["wind"].deg
+						"windMetric": data["wind"].speed,
+						"windMPH": Math.round((data["wind"].speed)/0.44704)
 					};
-					displayWeather(weatherData);
+					displayWeather(weatherData); //app.js.61
+					displayExtra(weatherData); //app.js.69
 					return(weatherData);
 				}
 			}) //close ajax method
 		}, //close getCurrentPosition success callback
-		displayError,
+		displayError, //app.js.50
 		{enableHighAccuracy:true, timeout:timeoutVal, maximumAge:0}
 		) //close getCurrentPosition method
 	} else { //If browser doesn't support geolocation
-		$(".current").html("We are having trouble finding you at the moment, please enter your zip code below to get your current weather");
+		$("#noLoc").html("We are having trouble finding you at the moment, please enter your zip code below to get your current weather");
 	}
 }
 
-//Error handling for geolocation
+//Error handling for geolocation //Called at app.js.41
 function displayError(error) {
   var errors = { 
-    1: 'Permission denied',
-    2: 'Position unavailable',
-    3: 'Request timeout'
+    1: 'Geolocation permission denied. Please enter your zip code below [E.01]',
+    2: 'Automatic location unavailable at this time, please enter your zip code below [E.02]',
+    3: 'Automatic location unavailable at this time, please enter your zip code below [E.03]'
   };
-  $(".current").html("Error: " + errors[error.code]);
+  $("#noLoc").html(errors[error.code]);
+  console.log(errors[error.code]);
 }
 
-//Add weatherData to HTML, called inside getWeather > getCurrentPosition > ajax() > success callback
-function displayWeather(info){
+//Add weatherData to HTML //Called at app.js.35
+function displayWeather(info) {
 	$("#local").html(info.city + ", " + info.country);
 	$("#temp").html(info.tempC + "°C").addClass("metric");
 	$("#skies").html(info.description);
-	changeBackground(info.icon);
+	changeBackground(info.icon); //Defined at app.js.76
 }
 
-//Update background image depending on [weather][0].description, called inside displayWeather
+//Add additional weatherData on desktop view //Call at app.js.36
+function displayExtra(info) {
+	$("#pressure").html(info.pressure + " hpa");
+	$("#humidity").html(info.humidity + "%");
+	$("#wind").html(info.windMetric + " m/s");
+}
+
+//Update background image depending on [weather][0].description //Called at app.js.65
 function changeBackground(type) {
 	switch(type) {
 			  case "01d":
@@ -103,24 +116,24 @@ function changeBackground(type) {
 			}
 }
 
+//Toggle Celsius/Fahrenheit when temp is clicked, also updates wind speed units
 $("#temp").on("click", function(){
 	if ($("#temp").hasClass("metric")){
 		$("#temp").html(weatherData.tempF + "°F").removeClass("metric");
+		$("#wind").html(weatherData.windMPH + " mph");
 	} else {
 		$("#temp").html(weatherData.tempC + "°C").addClass("metric");
+		$("#wind").html(weatherData.windMetric + " m/s");
 	}
 });
 
-function validZip(el) {
-	var zipPattern = /^(\d{5})?$/;
-	return(zipPattern.test(el));
-}
-
+//Function to check weather by zip code lookup //Called in index.html.48
 function getWeatherZip(zipCode) {
-	console.log(zipCode);
-	if (validZip(zipCode)) {
+	$("#noLoc").html("");
+	if (validZip(zipCode)) { //Defined at app.js.165
 		$.ajax({
 			url: "http://api.openweathermap.org/data/2.5/weather",
+			// data: to be sent to API server
 			data: {
 				zip: zipCode,
 				units: "metric",
@@ -129,6 +142,7 @@ function getWeatherZip(zipCode) {
 			async: true,
 			cache: false,
 			success: function(data) {
+				//data to ask from API server
 				weatherData = {
 					"city": data["name"],
 					"country": data["sys"].country,
@@ -138,18 +152,34 @@ function getWeatherZip(zipCode) {
 					"icon": data["weather"][0].icon,
 					"pressure": data["main"].pressure,
 					"humidity": data["main"].humidity,
-					"windSpeed": data["wind"].speed,
-					"windDir": data["wind"].deg
+					"windMetric": data["wind"].speed,
+					"windMPH": Math.round((data["wind"].speed)/0.44704)
 				};
-				displayWeather(weatherData);
+				displayWeather(weatherData); //Defined at app.js.61
+				displayExtra(weatherData); //Defined at app.js.69
 				return(weatherData);
+			},
+			error: function() {
+				$("noZip").html("Something went wrong. Please try again later");
 			}
-		})
+		}) //close ajax
 	} else {
-		$(".error").html("Please enter a 5-digit zip code");
-	}
+		$("#noZip").html("Please enter a 5-digit zip code");
+	} // close if validZip
 }
 
+//Check if user entered 5 numbers //Called at app.js.132
+function validZip(el) {
+	var zipPattern = /^(\d{5})?$/;
+	return(zipPattern.test(el));
+}
 
+//Prevent browser refresh on zip code submit
+$("#zipForm").submit(function(e) {
+	e.preventDefault();
+});
+
+//Tries to get weather by geolocation on page load
 getWeather();
+
 
